9c67700096bc7c3a0e5263f4221667f503b86449
Bryan Gonnella <bryan@gonnella.dev>
2025-05-14 03:05:57 -0700

    Updated login check on Home

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,8 +1,18 @@
 import { useAuth } from '@/contexts/auth';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
   const { username } = useAuth()
-  if (typeof localStorage != 'undefined') localStorage.removeItem("redirect")
+  const router = useRouter()
+  const [isLoading, setIsLoading] = useState(true)
+  useEffect(() => {
+      setIsLoading(false)
+  }, [username, router])
+  if (isLoading) {
+    return null
+  }
+  localStorage.removeItem("redirect")
   return (
     <div className="flex flex-grow items-center justify-center">
         <h2 className="text-black bg-white p-12 text-5xl font-bold w-1/3 text-center border shadow-lg">
